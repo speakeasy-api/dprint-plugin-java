c9197a362220da847220b09f98aa5d5f5e67e391
Gautier Ben Aïm <benaim.gautier@gmail.com>
2026-03-05 17:51:28 +0100

    remove random files

--- TestCase.java
+++
@@ -1,9 +0,0 @@
-public class TestCase {
-    void method() {
-        JCRNodeWrapper mountPointNode = jcrMountPointNode.getVirtualMountPointNode();
-        final JCRStoreProvider provider = externalProviderFactory.mountProvider(mountPointNode);
-        if (!provider.isAvailable(true)) {
-            System.out.println();
-        }
-    }
-}
